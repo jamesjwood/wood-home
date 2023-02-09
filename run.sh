81411101c4df4e1afbb9114a0e5730acaaff3071
jamesjwood <james.wood@datatransparency.com>
2023-02-09 10:28:39 -0800

    initial

---
+++ run.sh
@@ -0,0 +1,20 @@
+# update
+sudo apt update -y
+sudo apt upgrade -y
+
+# install tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
+tailscale up
+
+# install docker
+curl -fsSL test.docker.com -o get-docker.sh && sh get-docker.sh
+sudo usermod -aG docker ${USER}
+
+docker run hello-world
+
+# install docker-compose
+sudo apt-get update
+sudo apt-get install docker-compose-plugin
+
+# run
+docker compose up
